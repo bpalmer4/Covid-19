e1246d8bd5493d66d7c5c43fb60ae521c1246d5c
bpalmer4 <palmer.bryan@gmail.com>
2021-01-04 09:32:37 +1100

    minor updates

--- bin/plotstuff.py
+++ bin/plotstuff.py
@@ -686,7 +686,7 @@ def plot_weekly(daily, mode, data_quality, dfrom="2020-01-21", **kwargs):
             name,
             'week',
             dfrom, 
-            title=f'{name}: COVID-19 {mode.title()}',
+            title=f'COVID-19 {mode.title()} in {name}',
             rfooter=data_quality[name],
             lfooter=f'Total {mode.lower()}: '
                     f'{int(total[name]):,}; '
